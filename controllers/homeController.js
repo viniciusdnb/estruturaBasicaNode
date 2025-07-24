3570ca453f91e8788aba391563918de58b2188d4
Vinicius Henrique costa da silva <71468843+viniciusdnb@users.noreply.github.com>
2025-07-24 15:20:39 -0300

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,12 @@
 module.exports = {
-    index: function(req, res){
+    index: async function(req, res){
+       const usersModel = require('../model/models/usersModel');
+       const user = await usersModel.findAll({where:{id:1}});
+       console.log(JSON.stringify(user, null));
         res.render('views/home/index');
-    }
+    },
+  
+    
+
+
 }
